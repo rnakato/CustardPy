6a614eb0d07a0fe756eab680e970761167adfd3e
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2022-07-21 14:17:20 +0900

    test

--- tutorial/00.GetData.sh
+++ tutorial/00.GetData.sh
@@ -2,4 +2,4 @@ mkdir -p fastq/Hap1-A
 fastq-dump --split-files --gzip SRR5266584 -O fastq/Hap1-A
 
 # insteadn, download the paired fastq files from our Google Drive:
-# https://drive.google.com/file/d/1j_03GP_iE3E_BQDx1LGVu_qgfYCg8nxA/view?usp=sharing
+# https://drive.google.com/file/d/1Clo_sh3n4r82lp3ynkmUdLzcGx3gpu5Q/view?usp=sharing
--- tutorial/11.GenerateHic_cooler.sh
+++ tutorial/11.GenerateHic_cooler.sh
@@ -4,34 +4,36 @@ gt=/work/Database/UCSC/$build/genome_table
 index_bwa=/work/Database/bwa-indexes/UCSC-$build
 ncore=64
 
+#fq1=fastq/Hap1-A/SRR5266584_1.fastq.gz
+#fq2=fastq/Hap1-A/SRR5266584_2.fastq.gz
+#idir=Cooler_$build/Hap1-A
+fq1=fastq/Control/SRR5952305_1.fastq.gz
+fq2=fastq/Control/SRR5952305_2.fastq.gz
+odir=Cooler_$build/Control
+
 sing="singularity exec --bind /work,/work2 /work/SingularityImages/custardpy.0.1.0.sif"
+ncore=64
+Ddir=/work/Database/Database_fromDocker/Ensembl-GRCh38
+chromap_index=$Ddir/chromap-indexes/genome
+genome=$Ddir/genome.fa
 
-fq1=fastq/Hap1-A/SRR5266584_1.fastq.gz
-fq2=fastq/Hap1-A/SRR5266584_2.fastq.gz
-idir=Cooler_$build/Hap1-A
-mkdir -p $idir/bam $idir/pairs $idir/log $idir/temp
-$sing bwa mem -5SP -T0 -t $ncore $index_bwa $fq1 $fq2 | samtools sort -@2 > $idir/bam/mapped.bwa.sort.bam 2> $idir/log/bwa.log
+mkdir -p $odir/pairs $odir/log
 
-$sing pairtools parse --min-mapq 30 --walks-policy 5unique --max-inter-align-gap 30 --nproc-in 8 --nproc-out 8 --chroms-path $gt $idir/bam/mapped.bwa.sort.bam \
-       | pairtools sort --nproc 8 --tmpdir=$idir/temp \
-       | pairtools dedup --nproc-in 8 --nproc-out 8 --mark-dups --output-stats $idir/pairs/mapped.bwa.stats.txt \
-       | pairtools split --nproc-in 8 --nproc-out 8 --output-pairs $idir/pairs/mapped.bwa.pairs --output-sam - \
-       | samtools sort -@16 -T $idir/temp/temp.bam > $idir/bam/mapped.final.sort.bam
-rm -rf $idir/temp
+echo "start mapping..."
+#$sing chromap --preset hic -t $ncore --remove-pcr-duplicates -x $chromap_index -r $genome -1 $fq1 -2 $fq2 -o $odir/pairs/mapped.rmdup.pairs 2> $odir/log/chromap.rmdup.log
+echo "parse by pairtools..."
+enzyme=HindIII
+restrictionsite=/Cooler-restriction_sites/${enzyme}_resfrag_$build.bed
 
-echo "samtools indexing..."
-samtools index $idir/bam/mapped.final.sort.bam
-python /work/sakata/python/get_qc.py -p $idir/pairs/mapped.bwa.stats.txt > $idir/pairs.stats.txt
-
-$sing bgzip $idir/pairs/mapped.bwa.pairs
-mkdir -p $idir/hic $idir/cool
-pair=$idir/pairs/mapped.bwa.pairs.gz
-$sing pairix -p pairs $pair
+$sing gunzip -c $odir/pairs/mapped.rmdup.pairs.gz | $sing fragment_4dnpairs.pl -a - $odir/pairs/mapped.ff.pairs $restrictionsite
+$sing bgzip  -f $odir/pairs/mapped.ff.pairs
+$sing pairix -f $odir/pairs/mapped.ff.pairs.gz
+pair=$odir/pairs/mapped.ff.pairs.gz
+$sing genCoolHiC_from_pairs Control $odir $pair $gt
 
 exit
 
 enzyme=HindIII
-restrictionsite=/Cooler-restriction_sites/${enzyme}_resfrag_$build.bed
 mkdir -p $idir/hic $idir/cool
 
 $sing bgzip -d -c $input_pairs | $sing fragment_4dnpairs.pl -a - $idir/pairs/mapped.bwa.ff.pairs $restrictionsite
@@ -49,6 +51,30 @@ $sing Rscript plot.r 4 $idir/qc.${tool}_report
 $sing cooler cload pairix -p $ncore -s $max_split $gt:$binsize_min $pair $idir/cool/$prefix.$tool.cool
 $sing cooler balance -p $ncore $idir/cool/$prefix.$tool.cool
 $sing run-cool2multirescool.sh -i $idir/cool/$prefix.$tool.cool -p $ncore -o $idir/cool/$prefix.$tool -u $binsize_multi
+
+
+exit
+### bwa
+mkdir -p $idir/bam $idir/pairs $idir/temp
+$sing bwa mem -5SP -T0 -t $ncore $index_bwa $fq1 $fq2 | samtools sort -@2 > $idir/bam/mapped.bwa.sort.bam
+
+$sing pairtools parse --min-mapq 30 --walks-policy 5unique --max-inter-align-gap 30 --nproc-in 8 --nproc-out 8 --chroms-path $gt $idir/bam/mapped.bwa.sort.bam \
+       | pairtools sort --nproc 8 --tmpdir=$idir/temp \
+       | pairtools dedup --nproc-in 8 --nproc-out 8 --mark-dups --output-stats $idir/pairs/mapped.bwa.stats.txt \
+       | pairtools split --nproc-in 8 --nproc-out 8 --output-pairs $idir/pairs/mapped.bwa.pairs --output-sam - \
+       | samtools sort -@16 -T $idir/temp/temp.bam > $idir/bam/mapped.final.sort.bam
+rm -rf $idir/temp
+
+echo "samtools indexing..."
+samtools index $idir/bam/mapped.final.sort.bam
+python /work/sakata/python/get_qc.py -p $idir/pairs/mapped.bwa.stats.txt > $idir/pairs.stats.txt
+
+mkdir -p $idir/hic $idir/cool
+pair=$idir/pairs/mapped.bwa.pairs.gz
+$sing pairix -p pairs $pair
+
+#$sing genCoolHiC_from_pairs $idir/hic/q30 $idir $pair $gt
+
 
 exit
 
