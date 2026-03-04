f98e5dff3da815eebbb1d69c48abf93a0bab6872
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2026-03-04 16:26:20 +0900

    Update: v3.0.1

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,11 +1,11 @@
 reponame=custardpy
 
-tag=3.0.0
+tag=3.0.1
 docker build -f Dockerfile.$tag -t rnakato/$reponame:$tag . #--no-cache
 #echo "docker save -o $reponame-$tag.tar rnakato/$reponame:$tag"
 #docker save -o $reponame-$tag.tar rnakato/$reponame:$tag
 #singularity build -F /work3/SingularityImages/$reponame.$tag.sif docker-archive://$reponame-$tag.tar
-apptainer build -F /work3/SingularityImages/$reponame.$tag.sif docker-daemon://rnakato/$reponame:$tag
+#apptainer build -F /work3/SingularityImages/$reponame.$tag.sif docker-daemon://rnakato/$reponame:$tag
 
 exit
 docker push rnakato/$reponame:$tag
