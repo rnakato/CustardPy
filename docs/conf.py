01c6d1008826cbfd511946e0eb449d575f777f21
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2026-03-20 12:10:42 +0900

    Update: v3.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -91,7 +91,8 @@ html_logo = "_static/img/CustardPy_logo.jpg"
 # a list of builtin themes.
 #
 #html_theme = 'sphinx_rtd_theme'
-html_theme = "nature"
+html_theme = "sphinx_book_theme"
+#html_theme = "nature"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
