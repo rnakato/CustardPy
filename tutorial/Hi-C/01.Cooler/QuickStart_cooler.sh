d9684995e5f65572ab16970480cb68b348e00b6c
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2026-03-20 17:54:29 +0900

    Update: v3.1.0

--- tutorial/Hi-C/01.Cooler/QuickStart_cooler.sh
+++ tutorial/Hi-C/01.Cooler/QuickStart_cooler.sh
@@ -7,20 +7,27 @@ genome=genome.$build.fa
 ncore=64
 enzyme=MboI
 
-sing="singularity exec --bind /work,/work2,/work3 /work/SingularityImages/custardpy.3.1.0.sif"
-#sing="singularity exec custardpy.sif"
+sing="apptainer exec --bind /work,/work2,/work3 /work/SingularityImages/custardpy.3.1.0.sif"
+sing_gpu="apptainer exec --nv --bind /work,/work2,/work3 /work/SingularityImages/custardpy.3.1.0.sif"
 
 for cell in Control siCTCF siRad21 siNIPBL
 do
+    # generate .cool and .hic files
     $sing custardpy_cooler -g $gt -f $genome -b $build -e $enzyme -i $index_bwa -p $ncore fastq/$cell $cell
 
+    # downstream analysis of .cool files (eigenvector, insulation score, loop calling, etc.)
     odir=CustardPyResults_Hi-C/Cooler_$build/$cell
-
     cool=$odir/cool/cooler.dedup.q30.multires.cool
-    pair=$odir/pairs/dedup.bwa.q30.pairs.gz
     $sing custardpy_process_cool -t $ncore -g $gt -p $pair -f $genome $cool $odir $cell
 
+    ## loop calling with fithic (take long time, so run separately)
+    resolutions_fithic=25000
+    pair=$odir/pairs/dedup.bwa.q30.pairs.gz
+    odir_fithic=$odir/loops/fithic/$resolutions_fithic
+#    $sing run_fithic.sh -g $gt $pairfile $odir_fithic $label $resolution | tee $odir/log/fithic.$resolution.log
+
+    # (Optional) Juicer analysis (requires GPU, so run separately)
     hic=$odir/hic/contact_map.q30.hic
     norm=SCALE
-#    $sing custardpy_process_hic -p $ncore -n $norm -g $gt -a $gene $hic $odir
+#    $sing_gpu custardpy_process_hic -p $ncore -n $norm -g $gt -a $gene $hic $odir
 done
