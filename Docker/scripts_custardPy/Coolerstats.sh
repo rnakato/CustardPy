c8bd1042d6d92839fec24370ccd5703e4216ecc7
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2026-06-15 11:40:02 +0900

    Update v3.5.2

--- Docker/scripts_custardPy/Coolerstats.sh
+++ Docker/scripts_custardPy/Coolerstats.sh
@@ -31,10 +31,7 @@ outputstats(){
 	echo -en "`cat $odir/$cell/InsulationScore/25000/TADs.w100000.bed | wc -l`\t"
 	echo -en "`cat $odir/$cell/loops/cooltools/dots.10000.tsv | wc -l`\t"
 	echo -en "`zcat $odir/$cell/loops/fithic/25000/${cell}_25000.merged.gz | wc -l`\t"
-	echo -en "`zcat $odir/$cell/loops/mustache/loop.cool.5kb.tsv | wc -l`\n"
-    
-     
-	grep -v \# $odir/$cell/loops/$norm/merged_loops.bedpe | wc -l
+	echo -en "`cat $odir/$cell/loops/mustache/loop.cool.5kb.tsv | wc -l`\n"
     done
 }
 
